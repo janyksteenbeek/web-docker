397ea51a2cd413712cf29793c2f8c41421051120
Janyk Steenbeek <janyk@webmethod.nl>
2023-03-30 14:00:54 +0200

    Update startup.sh

--- .deployment/startup.sh
+++ .deployment/startup.sh
@@ -1,9 +1,32 @@
 #!/bin/sh
+
+ARTISAN_FILE="/app/artisan"
+
+# Check if artisan exists and is executable
+if [[ -x "$(command -v php)" && -x "${ARTISAN_FILE}" ]]; then
+    # Check if SKIP_OPTIMIZE_COMMANDS is NOT set
+    if [[ -z "${SKIP_OPTIMIZE_COMMANDS}" ]]; then
+        echo "Running artisan commands for production environment..."
+
+        # Run recommended artisan commands for production
+        php ${ARTISAN_FILE} config:cache
+        php ${ARTISAN_FILE} route:cache
+        php ${ARTISAN_FILE} view:cache
+        php ${ARTISAN_FILE} optimize
+    else
+        echo "Skipping optimization commands due to the SKIP_OPTIMIZE_COMMANDS environment variable."
+    fi
+else
+    echo "Artisan or PHP not found. Skipping optimization commands."
+fi
+
+
 echo "Starting PHP FPM in daemon mode"
 php-fpm -D
-# while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
+
 echo "Testing NGINX"
 nginx -t
 cat /etc/nginx/nginx.conf
+
 echo "Starting NGINX"
 nginx
